297c26ddd449646c3471590e95b2bc02ee56b303
Mehmet Fethi Meşe <m.fethimese@outlook.com>
2020-07-23 13:58:09 +0300

    Multiton and NullObject design patterns added

---
+++ Multiton/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Multiton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camera camera1 = Camera.CreateSingleton("NIKON");
+            Camera camera2 = Camera.CreateSingleton("CANON");
+            Camera camera3 = Camera.CreateSingleton("NIKON");
+            Camera camera4 = Camera.CreateSingleton("NIKON");
+
+
+            Console.WriteLine("Camera:{0}, Id: {1}",camera1.GetName(), camera1.Id);
+            Console.WriteLine("Camera:{0}, Id: {1}", camera2.GetName(), camera2.Id);
+            Console.WriteLine("Camera:{0}, Id: {1}", camera3.GetName(), camera3.Id);
+            Console.WriteLine("Camera:{0}, Id: {1}", camera4.GetName(), camera4.Id);
+
+            Console.Read();
+        }
+    }
+
+    class Camera
+    {
+        static Dictionary<string, Camera> _cameras = new Dictionary<string, Camera>();
+        private static object _lock = new object();
+        public Guid Id { get; set; }   
+
+        private Camera()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public static Camera CreateSingleton(string name)
+        {
+            lock (_lock)
+            {
+                if (!_cameras.ContainsKey(name))
+                {
+                    _cameras.Add(name, new Camera());
+                }
+            }
+
+            return _cameras[name];
+        }
+
+        public string GetName()
+        {
+            return _cameras.FirstOrDefault(x => x.Value == this).Key;
+        }
+    }
+}
---
+++ NullObject/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductServiceTests test = new ProductServiceTests();
+            test.Saving_Test();
+
+            Console.Read();
+        }
+    }
+
+    class ProductService
+    {
+        private ILogger _logger;
+
+        public ProductService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log();
+            Console.WriteLine("Saved");
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log4Net");
+        }
+    }
+
+    class EdLogger: ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with EdLogger");
+        }
+    }
+
+    class StubLogger : ILogger
+    {
+        private static StubLogger _stubLogger;
+        private StubLogger() { }
+        private static object _lock = new object();
+
+        public static StubLogger CreateSingleton()
+        {
+            lock (_lock)
+            {
+                _stubLogger = _stubLogger == null ? new StubLogger() : _stubLogger;
+            }
+            return _stubLogger;
+        }
+        public void Log()
+        {
+            
+        }
+    }
+
+    class ProductServiceTests
+    {
+        public void Saving_Test()
+        {
+            ProductService service = new ProductService(StubLogger.CreateSingleton());
+            service.Save();
+        }
+    }
+}
